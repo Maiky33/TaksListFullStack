7708de348a8568a27d7f67589c2919696f041940
unknown <maicolbarrerag@gmail.com>
2022-12-04 15:50:15 -0500

    update project

--- server/controllers/tasks.controllers.js
+++ server/controllers/tasks.controllers.js
@@ -6,50 +6,87 @@ import { connection } from "../db.js"; //importamos la coneccion a la base de da
 
 //obtener todas las tareas
 export const getTasks = async(req, res) => {
-  const [result] = await connection.query('SELECT * FROM task ORDER BY createAt ASC')
-  res.json(result);
+
+  try{  
+    const [result] = await connection.query('SELECT * FROM task ORDER BY createAt ASC')
+    res.json(result);
+  }catch(error){  
+    return res.status(500).json({message:"error Conection"})
+  }
+
 };
 
 
 //obtener todas las tareas
 export const getTask = async(req, res) => { 
-  const {id} = req.params
-  const [result] = await connection.query('SELECT title,description FROM task WHERE id = ?',id)
-  res.json(result)
+  try{  
+    const {id} = req.params
+    const [result] = await connection.query('SELECT title,description FROM task WHERE id = ?',id)
+
+    if(result.length === 0) return res.status(404).json({message:'Task Not Found'})
+    res.json(result[0])
+  }catch{ 
+    return res.status(500).json({message:"error Conection"})
+  }
 };
 
 
 //crear la tarea
 export const createTask = async (req, res) => {
 
-  const { title, description } = req.body;//resivimos de la peticon, por req.body los parametro title, decription   
+  try{  
 
-  const task = { //hacemos un objeto para poder usarlo en la peticion mysql y asi poder agregarlo a la base de datos
-    title,
-    description,
-  };
+    const { title, description } = req.body;//resivimos de la peticon, por req.body los parametro title, decription   
 
-  const [result] = await connection.query("INSERT INTO task SET ?", task);//hacemos la consulta usando la coneccion a la base de datos y hacemos la peticion mysql atravez de el metodo query,hacemos la peticion y le pasamos el objeto el cual queremos agregar a la base de dato
+    const task = { //hacemos un objeto para poder usarlo en la peticion mysql y asi poder agregarlo a la base de datos
+      title,
+      description,
+    };
 
-  res.json({//mandamos la respuesta con res.json, le sacamos el id por result.inserId ,el title y el decription 
+    const [result] = await connection.query("INSERT INTO task SET ?", task);//hacemos la consulta usando la coneccion a la base de datos y hacemos la peticion mysql atravez de el metodo query,hacemos la peticion y le pasamos el objeto el cual queremos agregar a la base de dato
 
-    id: result.insertId,
-    title,
-    description,
-  });
+    res.json({//mandamos la respuesta con res.json, le sacamos el id por result.inserId ,el title y el decription 
+
+      id: result.insertId,
+      title,
+      description,
+    });
+
+  }catch{ 
+    return res.status(500).json({message:"error Conection"})
+  }
 };
 
 
 
 // actualizar tarea
-export const updateTask = (req, res) => {
-  res.send("actualizando tarea");
+export const updateTask = async (req, res) => {
+  try{  
+    const result = await connection.query('UPDATE task SET ? WHERE id = ?',[
+      req.body,
+      req.params.id
+    ]);
+    res.json(result)
+  }catch{ 
+    return res.status(500).json({message:"error Conection"})
+  }
 };
 
 
 //eliminar tarea 
-export const deleteTask = (req, res) => { 
-  res.send("eliminando tarea");
+export const deleteTask = async(req, res) => { 
+  
+  try{  
+    const {id}= req.params; 
+    const [result] = await connection.query('DELETE FROM task WHERE id = ?',id)
+
+    if(result.affectedRows === 0) return res.status(404).json({message:'Task Not Found'})
+    return res.sendStatus(204);
+    
+  }catch{ 
+    return res.status(500).json({message:"error Conection"})
+  }
+
 };
 
 
