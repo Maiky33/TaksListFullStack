5513e9d38bdb55f8a1ce073a6e5e3de9a4ca59bd
Maiky33 <maicolbarrerag@gmail.com>
2023-01-12 15:49:25 -0500

    P

--- server/index.js
+++ server/index.js
@@ -1,21 +1,16 @@
 import express from "express";
 import cors from 'cors';
 import { PORT } from "./config.js";
-import {dirname,join} from 'path'
-import {fileURLToPath} from 'url'
 import indexRoutes from "./routes/index.routes.js";
 import taksRoutes from "./routes/task.routes.js";
 
 const app = express();
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(express.json());
 app.use(cors());//para poder comunicarnos desde backend
 
 app.use(indexRoutes);
 app.use(taksRoutes);
-app.use(express.static(join(__dirname, '../client/build')))
 
 app.listen(PORT);
 console.log(`server is running on ${PORT}`);
