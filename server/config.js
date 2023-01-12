0315173a41b316405efb18e9b775fecf3a6cedc6
Maiky33 <maicolbarrerag@gmail.com>
2023-01-12 18:42:58 -0500

    last commit

--- server/config.js
+++ server/config.js
@@ -1,2 +1,8 @@
 //crear el puerto en config
-export const PORT = 4000;
+export const PORT = process.env.PORT || 4000;
+
+export const DB_HOST = process.env.DB_HOST || "localhost"
+export const DB_PORT = process.env.DB_PORT || 3306
+export const DB_USER = process.env.DB_USER || "root"
+export const DB_PASSWORD = process.env.DB_PASSWORD || "root123"
+export const DB_NAME = process.env.DB_NAME || "taskdb"
--- server/db.js
+++ server/db.js
@@ -1,9 +1,16 @@
 import { createPool } from 'mysql2/promise' //CON ESTE POOL VAMOS A PODER EJECUTAR CONSULTAS ATRAVEZ DEL METODO LLAMADO QUERY 
+import {    
+    DB_HOST,
+    DB_PORT,
+    DB_USER,
+    DB_PASSWORD,
+    DB_NAME
+} from './config.js'
 
 export const connection = createPool({ //IMPORTANTE PONER EN LA CONSTANTE DE CREATEPOOL ALGUNAS PROPIEDADES DE LA BASE DE DATOS
-    host: process.env.HOST || "localhost",
-    port: process.env.PORT || "3306",
-    user: process.env.USER || "root",
-    password: process.env.PASSWORD || "root123",
-    database: process.env.DATABASE || "taskdb"
+    host:DB_HOST,
+    port:DB_PORT,
+    user:DB_USER,
+    password:DB_PASSWORD,
+    database:DB_NAME
 })
