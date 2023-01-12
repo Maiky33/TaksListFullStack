76a464fe4037bfcd43d2352220829ff25b792a92
Maiky33 <maicolbarrerag@gmail.com>
2023-01-12 16:50:06 -0500

    change database

--- server/db.js
+++ server/db.js
@@ -1,9 +1,9 @@
 import { createPool } from 'mysql2/promise' //CON ESTE POOL VAMOS A PODER EJECUTAR CONSULTAS ATRAVEZ DEL METODO LLAMADO QUERY 
 
 export const connection = createPool({ //IMPORTANTE PONER EN LA CONSTANTE DE CREATEPOOL ALGUNAS PROPIEDADES DE LA BASE DE DATOS
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'root123',
-    database: 'taskdb'
+    host: process.env.HOST || "localhost",
+    port: process.env.PORT || "3306",
+    user: process.env.USER || "root",
+    password: process.env.PASSWORD || "root123",
+    database: process.env.DATABASE || "taskdb"
 })
