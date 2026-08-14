e5d5b1837e9bcb1f0c795d6d0f30fe952842f72b
Maiky33 <maicolbarrerag@gmail.com>
2026-08-14 13:54:05 -0500

    test deploy

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -2,25 +2,25 @@
 
 import axios from "axios"; //usamos axios para hacer la coneccion al backend
 
-
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:4000";
 
 export const getTasksRequest = async () =>
-  await axios.get("http://localhost:4000/tasks");
+  await axios.get(`${API_URL}/tasks`);
 
 export const getTaskRequest = async (id) =>
-  await axios.get(`http://localhost:4000/tasks/${id}`)
+  await axios.get(`${API_URL}/tasks/${id}`)
 
 export const createTaskRequest = async (task) => //creamos una constante que resibe la tarea que vamos a enviar al backend
-  await axios.post("http://localhost:4000/tasks", task);// usamos axios.post para enviar la tarea/crearla, como primer parametro le pasamos el servidor donde esta ubicado y le pasamos como segundo parametro la tarea
+  await axios.post(`${API_URL}/tasks`, task);// usamos axios.post para enviar la tarea/crearla, como primer parametro le pasamos el servidor donde esta ubicado y le pasamos como segundo parametro la tarea
 
 export const deletedTasksRequest = async (id) => 
-  await axios.delete(`http://localhost:4000/tasks/${id}`, id);
+  await axios.delete(`${API_URL}/tasks/${id}`, id);
 
 export const updateTaksRequest = async (id, newFields) => 
-  await axios.put(`http://localhost:4000/tasks/${id}`,newFields) //para actualiar la tarea enviamos el id y los nuevos valores
+  await axios.put(`${API_URL}/tasks/${id}`,newFields) //para actualiar la tarea enviamos el id y los nuevos valores
 
 export const updateDoneRequest = async (id, done)=>
-  await axios.put(`http://localhost:4000/tasks/${id}`,{ // enviamos el id y el done para asi cambiarlo
+  await axios.put(`${API_URL}/tasks/${id}`,{ // enviamos el id y el done para asi cambiarlo
     done,
   })
 
